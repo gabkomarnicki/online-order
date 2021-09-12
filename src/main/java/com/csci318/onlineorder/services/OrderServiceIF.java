98eeebd03d82592e0f23bfaec21163bcf6791ec4
Gabriel Komarnicki <20436870+gabkomarnicki@users.noreply.github.com>
2021-09-13 09:22:11 +1000

    Created OrderServiceIF

---
+++ src/main/java/com/csci318/onlineorder/services/OrderServiceIF.java
@@ -0,0 +1,15 @@
+package com.csci318.onlineorder.services;
+
+import com.csci318.onlineorder.models.Order;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
+
+public interface OrderServiceIF {
+
+    public abstract CollectionModel<EntityModel<Order>> all();
+    public abstract ResponseEntity<?> newOrder(Order newOrder);
+    public abstract EntityModel<Order> one(Long id);
+    public abstract ResponseEntity<?> replaceOrder(Order newOrder,Long id);
+    public abstract ResponseEntity<?> deleteOrder(Long id);
+}
