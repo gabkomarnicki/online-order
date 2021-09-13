021a73423d54fe483b652aecbb0d45429656b567
Gabriel Komarnicki <20436870+gabkomarnicki@users.noreply.github.com>
2021-09-13 18:11:40 +1000

    Renamed sample variables

--- src/main/java/com/csci318/onlineorder/controllers/LoadDatabase.java
+++ src/main/java/com/csci318/onlineorder/controllers/LoadDatabase.java
@@ -14,14 +14,11 @@ public class LoadDatabase {
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
     //loading database with data
     @Bean
-    CommandLineRunner initDB(OrderRepository repository) {
+    CommandLineRunner initDB(OrderRepository orderRepository) {
         return args -> {
-//            log.info("Loading... " + repository.save(new Customer("ABC Company", "221B Baker St", "United Kingdom")));
-//            log.info("Loading... " + repository.save(new Customer("DEF Company", "742 Evergreen Terrace", "United States of America")));
-//            log.info("Loading... " + contactRepository.save(new Contact("Mark", "Baggins", "021456378", "markFbil@gmail.com", "CEO")));
-//            log.info("Loading... " + contactRepository.save(new Contact("Phill", "Baggins", "049876512","frodlib@outlook.com", "President")));
-            log.info("Loading..." + repository.save(new Order("ABC Supplier", "microwave", "20")));
-            log.info("Loading..." + repository.save(new Order("FedEx", "microprocessor", "90")));
+            log.info("Loading... " + orderRepository.save(new Order("ABC Supplier", "microwave", "20")));
+            log.info("Loading... " + orderRepository.save(new Order("FedEx", "microprocessor", "90")));
+            log.info("Loading..." + orderRepository.save(new Order("ACME Delivery", "dishwasher", "70")));
         };
     }
 }
