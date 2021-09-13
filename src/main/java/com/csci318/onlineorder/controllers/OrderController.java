0107ae5c917c2f928c775bc6e8fb175f5be1558d
Gabriel Komarnicki <20436870+gabkomarnicki@users.noreply.github.com>
2021-09-13 17:13:45 +1000

    Refactored

--- src/main/java/com/csci318/onlineorder/controllers/OrderController.java
+++ src/main/java/com/csci318/onlineorder/controllers/OrderController.java
@@ -43,30 +43,11 @@ public class OrderController {
         return orderService.all();
     }
 
-//    @GetMapping("/orders")
-//    CollectionModel<EntityModel<Order>> all() {
-//
-//        List<EntityModel<Order>> orders = repository.findAll().stream()
-//                .map(assembler::toModel)
-//                .collect(Collectors.toList());
-//
-//        return CollectionModel.of(orders, linkTo(methodOn(OrderController.class).all()).withSelfRel());
-//    }
     //create new order
     @PostMapping("/orders")
     Order newOrder(@RequestBody Order newOrder) {
         return orderService.newOrder(newOrder);
     }
-
-//    @PostMapping("/orders")
-//    ResponseEntity<?> newOrder(@RequestBody Order newOrder) {
-//
-//        EntityModel<Order> entityModel = assembler.toModel(repository.save(newOrder));
-//
-//        return ResponseEntity
-//                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
-//                .body(entityModel);
-//    }
 
     //find order by id
     @GetMapping("/orders/{id}")
@@ -74,53 +55,15 @@ public class OrderController {
         return orderService.one(id);
     }
 
-//    @GetMapping("/orders/{id}")
-//    EntityModel<Order> one(@PathVariable Long id) {
-//
-//        Order order = repository.findById(id)
-//                .orElseThrow(() -> new OrderNotFoundException(id));
-//
-//        return assembler.toModel(order);
-//    }
-
     //update order by id
     @PutMapping("/orders/{id}")
     Order replaceOrder(@RequestBody Order newOrder, @PathVariable Long id) {
         return orderService.replaceOrder(newOrder, id);
     }
 
-//    @PutMapping("/orders/{id}")
-//    ResponseEntity<?> replaceOrder(@RequestBody Order newOrder, @PathVariable Long id) {
-//
-//        Order updatedOrder = repository.findById(id)
-//                .map(order -> {
-//                    order.setSupplier(newOrder.getSupplier());
-//                    order.setProduct(newOrder.getProduct());
-//                    order.setQuantity(newOrder.getQuantity());
-//                    return repository.save(order);
-//                })
-//                .orElseGet(() -> {
-//                    newOrder.setId(id);
-//                    return repository.save(newOrder);
-//                });
-//
-//        EntityModel<Order> entityModel = assembler.toModel(updatedOrder);
-//
-//        return ResponseEntity
-//                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
-//                .body(entityModel);
-//    }
     //delete order by id
     @DeleteMapping("/orders/{id{")
     void deleteOrder(@PathVariable Long id) {
         orderService.deleteOrder(id);
     }
-
-//    @DeleteMapping("/orders/{id}")
-//    ResponseEntity<?> deleteOrder(@PathVariable Long id) {
-//
-//        repository.deleteById(id);
-//
-//        return ResponseEntity.noContent().build();
-//    }
 }
