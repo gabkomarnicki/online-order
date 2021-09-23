744662ca11dcbdae754ef46386c55ffb9b10ce0d
Gabriel Komarnicki <20436870+gabkomarnicki@users.noreply.github.com>
2021-09-23 18:13:22 +1000

    Constructor and @EventListener for OrderEventHandler

--- src/main/java/com/csci318/onlineorder/services/OrderEventHandler.java
+++ src/main/java/com/csci318/onlineorder/services/OrderEventHandler.java
@@ -1,7 +1,21 @@
 package com.csci318.onlineorder.services;
 
+import com.csci318.onlineorder.models.OrdersEvent;
+import com.csci318.onlineorder.repositories.OrderRepository;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderEventHandler {
+
+    private final OrderRepository orderRepository;
+
+    public OrderEventHandler(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @EventListener
+    public void handle(OrdersEvent ordersEvent) {
+        orderRepository.save(order);
+    }
 }
